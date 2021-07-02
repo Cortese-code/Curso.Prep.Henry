a70f7cb81a4b8a192d02f82320187fa4540f18fa
cortese-coder <albertodiegocortese@gmail.com>
2021-07-02 14:33:14 -0300

    respuestas del prep henry

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,10 +1,10 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'Hola';
+const nuevaString = "Hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 45;
+var  nuevoNum = 45;
 
 // Crea una variable booleana:
 const nuevoBool = false;
@@ -31,6 +31,7 @@ function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
   return str;
+  
   
 }
 
